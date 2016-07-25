56b9d7c96c808f489eb5b792dc362f066f3a7503
mrmojica <mrmojica510@gmail.com>
2016-07-25 12:33:00 -0700

    added no button+random # function

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 var questionCounter = 0;
 
+
+    
+
+var ingredients = {
+    strong: ['Glug of rum', 'slug of whisky', 'splash of gin'],
+    salty: ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
+    bitter: ['Shake of bitters', 'splash of tonic', 'twist of lemon peel'],
+    sweet: ['Sugar cube', 'spoonful of honey', 'splash of cola'],
+    fruity: ['Slice of orange', 'dash of cassis', 'cherry on top']
+}
+
+
 var questionList = [{
            question: 'Do ye like yer drinks strong?',
            choices: ['Yes', 'No'],
-           correctAnswer: 0
+           correctAnswer: 0,
+           ingredients: ingredients.strong
        },{
            question: 'Do ye like it with a salty tang',
            choices: ['Yes', 'No'],
@@ -21,32 +34,49 @@ var questionList = [{
            choices: ['Yes', 'No'],
            correctAnswer: 0
        }];
-    
-
-var ingredients = {
-    strong: ['Glug of rum', 'slug of whisky', 'splash of gin'],
-    salty: ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
-    bitter: ['Shake of bitters', 'splash of tonic', 'twist of lemon peel'],
-    sweet: ['Sugar cube', 'spoonful of honey', 'splash of cola'],
-    fruity: ['Slice of orange', 'dash of cassis', 'cherry on top']
-}
 
 
 $(document).ready(function() {
+
     $(".user-choice").hide();
+   
+//Order Button 
     $(".order-button").click(function() {
        $(".start-page").hide();
        $(".user-choice").show();   
-
   for(var i = 0; i < questionList[questionCounter].question.length; i++) {
       $(".print-question").text(questionList[questionCounter].question);
 
   }
 
 
-
-
     });    
+
+//Nay! Button
+    $(".no-button").click(function() {
+        questionCounter++;
+         for(var i = 0; i < questionList[questionCounter].question.length; i++) {
+            $(".print-question").text(questionList[questionCounter].question);
+            $(".random-drink").empty();
+  }
+
+    });
+
+//Aye! Button
+    $(".yes-button").click(function() {
+        $(".random-drink").empty();
+        //alert(questionList[questionCounter].ingredients);
+        var rand = ingredients.strong[Math.floor(Math.random() * ingredients.strong.length)];
+        var html = '';
+        html += '<li>' + rand + '</li>';
+        $('.random-drink').append(html);
+
+    });
+
+
+
+
+
 
 }); 
 
@@ -58,35 +88,22 @@ $(document).ready(function() {
 
 /*
 User Clicks on Order Button - the welcome page disappears
-                            - first question appears
-                            - with two buttons, yes and no
-                            - if user click yes, display random drink
-                            - if use click no, move on to the next question
+                            Done- first question appears 
+                            Done- with two buttons, yes and no
+                            Working on it- if user click yes, display random drink
+                            Working on it- if use click no, move on to the next question
 
 */
 
 
 
 
-
-
 /************** Might Use **************************/
 
-// var newQuestion = new Bartender(questionList[questionCounter].question);
-// console.log(newQuestion); 
-
-
-  // 
-  //   var choiceCounter = 0;
-
-  //   function Bartender(question, choices) {
-  //       this.question = question;
-  //       this.choices = choices;
-
-
-  //   }
-
 //create a constructor function that selects ingredients
+
+
+
 
 
 
