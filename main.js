785f09af1ffb60abee2cbbd63de67c271c4048a3
0e0 <ejersonb@gmail.com>
2016-07-25 13:56:25 -0500

    Work on Nay button

--- main.js
+++ main.js
@@ -1,53 +1,27 @@
-//Notes
+var questionCounter = 0;
 
-/*
-Create Questions:
-
-Ingredient Object:
-
-Create a random drink
-
-
-
-
-*/
-
-
-
-
-
-    var questionList = [
-
-        {
-            question: 'Do ye like yer drinks strong?',
-            choices: ['Yes', 'No'],
-            correctAnswer: 0
-        },
-
-        {
-            question: 'Do ye like it with a salty tang',
-            choices: ['Yes', 'No'],
-            correctAnswer: 0
-        },
-
-        {
-            question: 'Are ye a lubber who likes it bitter',
-            choices: ['Yes', 'No'],
-            correctAnswer: 0
-        },
-
-        {
-            question: 'Would ye like a bit of sweetness with yer poison?',
-            choices: ['Yes', 'No'],
-            correctAnswer: 0
-        },
-
-        {
-            question: 'Are ye one for a fruity finish?',
-            choices: ['Yes', 'No'],
-            correctAnswer: 0
-        }
-    ];
+var questionList = [{
+           question: 'Do ye like yer drinks strong?',
+           choices: ['Yes', 'No'],
+           correctAnswer: 0
+       },{
+           question: 'Do ye like it with a salty tang',
+           choices: ['Yes', 'No'],
+           correctAnswer: 0
+       },{
+           question: 'Are ye a lubber who likes it bitter',
+           choices: ['Yes', 'No'],
+           correctAnswer: 0
+       },{
+           question: 'Would ye like a bit of sweetness with yer poison?',
+           choices: ['Yes', 'No'],
+           correctAnswer: 0
+       },{
+           question: 'Are ye one for a fruity finish?',
+           choices: ['Yes', 'No'],
+           correctAnswer: 0
+       }];
+    
 
 var ingredients = {
     strong: ['Glug of rum', 'slug of whisky', 'splash of gin'],
@@ -57,29 +31,62 @@ var ingredients = {
     fruity: ['Slice of orange', 'dash of cassis', 'cherry on top']
 }
 
+
+$(document).ready(function() {
+    $(".user-choice").hide();
+    $(".order-button").click(function() {
+       $(".start-page").hide();
+       $(".user-choice").show();   
+
+  for(var i = 0; i < questionList[questionCounter].question.length; i++) {
+      $(".print-question").text(questionList[questionCounter].question);
+
+  }
+
+
+
+
+    });    
+
+}); 
+
+
+
+
+
+/*************************Notes**********************/
+
+/*
+User Clicks on Order Button - the welcome page disappears
+                            - first question appears
+                            - with two buttons, yes and no
+                            - if user click yes, display random drink
+                            - if use click no, move on to the next question
+
+*/
+
+
+
+
+
+
+/************** Might Use **************************/
+
+// var newQuestion = new Bartender(questionList[questionCounter].question);
+// console.log(newQuestion); 
+
+
+  // 
+  //   var choiceCounter = 0;
+
+  //   function Bartender(question, choices) {
+  //       this.question = question;
+  //       this.choices = choices;
+
+
+  //   }
+
 //create a constructor function that selects ingredients
-
-
-    var questionCounter = 0;
-    var choiceCounter = 0;
-
-    function Bartender(question, choices) {
-    	this.question = question;
-    	this.choices = choices;
-
-
-    }
-
-
-
-var newQuestion = new Bartender(questionList[questionCounter].question);
-	
-// get value of yes or no..
-console.log(newQuestion); 
-
-
-
-
 
 
 
