06f5bc9599144e33cf60b30b3181036fe9de016f
mrmojica <mrmojica510@gmail.com>
2016-07-25 12:46:13 -0700

    added ingriedient counter to change drink by question

--- main.js
+++ main.js
@@ -1,72 +1,55 @@
 var questionCounter = 0;
-
-
-    
-
-var ingredients = {
-    strong: ['Glug of rum', 'slug of whisky', 'splash of gin'],
-    salty: ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
-    bitter: ['Shake of bitters', 'splash of tonic', 'twist of lemon peel'],
-    sweet: ['Sugar cube', 'spoonful of honey', 'splash of cola'],
-    fruity: ['Slice of orange', 'dash of cassis', 'cherry on top']
-}
-
+var ingredientCounter = 0;
 
 var questionList = [{
-           question: 'Do ye like yer drinks strong?',
-           choices: ['Yes', 'No'],
-           correctAnswer: 0,
-           ingredients: ingredients.strong
-       },{
-           question: 'Do ye like it with a salty tang',
-           choices: ['Yes', 'No'],
-           correctAnswer: 0
-       },{
-           question: 'Are ye a lubber who likes it bitter',
-           choices: ['Yes', 'No'],
-           correctAnswer: 0
-       },{
-           question: 'Would ye like a bit of sweetness with yer poison?',
-           choices: ['Yes', 'No'],
-           correctAnswer: 0
-       },{
-           question: 'Are ye one for a fruity finish?',
-           choices: ['Yes', 'No'],
-           correctAnswer: 0
-       }];
+    question: 'Do ye like yer drinks strong?',
+    ingredients: ['Glug of rum', 'slug of whisky', 'splash of gin']
+}, {
+    question: 'Do ye like it with a salty tang',
+    ingredients: ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon']
+}, {
+    question: 'Are ye a lubber who likes it bitter',
+    ingredients: ['Shake of bitters', 'splash of tonic', 'twist of lemon peel']
+}, {
+    question: 'Would ye like a bit of sweetness with yer poison?',
+    ingredients: ['Sugar cube', 'spoonful of honey', 'splash of cola']
+}, {
+    question: 'Are ye one for a fruity finish?',
+    ingredients: ['Slice of orange', 'dash of cassis', 'cherry on top']
+}];
 
 
 $(document).ready(function() {
 
     $(".user-choice").hide();
-   
-//Order Button 
+
+    //Order Button 
     $(".order-button").click(function() {
-       $(".start-page").hide();
-       $(".user-choice").show();   
-  for(var i = 0; i < questionList[questionCounter].question.length; i++) {
-      $(".print-question").text(questionList[questionCounter].question);
-
-  }
-
-
-    });    
-
-//Nay! Button
-    $(".no-button").click(function() {
-        questionCounter++;
-         for(var i = 0; i < questionList[questionCounter].question.length; i++) {
+        $(".start-page").hide();
+        $(".user-choice").show();
+        for (var i = 0; i < questionList[questionCounter].question.length; i++) {
             $(".print-question").text(questionList[questionCounter].question);
-            $(".random-drink").empty();
-  }
+
+        }
+
 
     });
 
-//Aye! Button
+    //Nay! Button
+    $(".no-button").click(function() {
+        questionCounter++;
+        ingredientCounter++;
+        for (var i = 0; i < questionList[questionCounter].question.length; i++) {
+            $(".print-question").text(questionList[questionCounter].question);
+            $(".random-drink").empty();
+        }
+
+    });
+
+    //Aye! Button
     $(".yes-button").click(function() {
         $(".random-drink").empty();
-        //alert(questionList[questionCounter].ingredients);
-        var rand = ingredients.strong[Math.floor(Math.random() * ingredients.strong.length)];
+        var rand = questionList[ingredientCounter].ingredients[Math.floor(Math.random() * questionList[ingredientCounter].ingredients.length)];
         var html = '';
         html += '<li>' + rand + '</li>';
         $('.random-drink').append(html);
@@ -78,7 +61,7 @@ $(document).ready(function() {
 
 
 
-}); 
+});
 
 
 
@@ -104,13 +87,10 @@ User Clicks on Order Button - the welcome page disappears
 
 
 
-
-
-
-
-
-
-
-
-
-
+// var ingredients = {
+//     strong: ['Glug of rum', 'slug of whisky', 'splash of gin'],
+//     salty: ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
+//     bitter: ['Shake of bitters', 'splash of tonic', 'twist of lemon peel'],
+//     sweet: ['Sugar cube', 'spoonful of honey', 'splash of cola'],
+//     fruity: ['Slice of orange', 'dash of cassis', 'cherry on top']
+// }
